ae1af4f7f21b82bd351f07b05947a5c1bafb9e92
Adam <adamkro@gmail.com>
2020-11-23 14:04:18 +0200

    added print tree

--- src/Testing.java
+++ src/Testing.java
@@ -3,9 +3,10 @@ import java.util.Arrays;
 public class Testing {
     public static void main(String[] args){
         AVLTree tree = new AVLTree();
-        tree.insert(1, "");
-        tree.insert(2, "");
-        tree.insert(3, "");
+        tree.insert(1, "1");
+        tree.insert(2, "2");
+        PrintBST.print(tree.getRoot());
+        tree.insert(3, "3");
         tree.insert(4, "");
         tree.insert(5, "d");
         tree.insert(6, "");
